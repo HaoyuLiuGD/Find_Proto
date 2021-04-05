428b0193418f945c9727fa92dea62b1339affc95
Haoyu Liu <seabornepizza47@gmail.com>
2021-04-05 22:47:15 +0800

    Add scene switch function (Press b)

---
+++ Find_Proto/Assets/Scripts/ChooseScene.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChooseScene : MonoBehaviour
+{
+    public GameObject cateye1;
+    public GameObject window1;
+    public GameObject computet1;
+
+    public Animator ani;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BackScene();
+    }
+
+    public void BackScene()
+    {
+        if(Input.GetKeyDown("b"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
+    }
+
+    public void SwitchAnimation()
+    {
+        ani.SetInteger("CatEyeIndex", 1);
+    }
+
+}
---
+++ Find_Proto/Assets/Scripts/GameManager_Main.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager_Main : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ToSceneChoose();
+    }
+
+    public void ToSceneChoose()
+    {
+        if (Input.GetKeyDown("b"))
+        {
+            //SceneManager.LoadScene("ChooseScene");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
